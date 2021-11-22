ca71ce84a1dd7bc3025c7cd73c9d89094e8a15c0
Zhixian Ong <zhixian@hotmail.com>
2021-11-22 13:51:25 +0800

    A better structured test

--- projects/travel-api/services/Counter.test.js
+++ projects/travel-api/services/Counter.test.js
@@ -1,10 +1,29 @@
 const counter = require('./counter');
 
-test('default counter', () => {
-    expect(counter.get()).toBe(0);
-});
+describe("Counter test", () => {
 
-test('increment', () => {
-    counter.increment();
-    expect(counter.get()).toBe(1);
+    beforeAll(() => {
+        // console.log("Before ANY tests are execute");
+    });
+
+    beforeEach(() => {
+        // console.log("Before EACH tests are execute");
+    });
+
+    afterEach(() => {
+        // console.log("After EACH tests are execute");
+    });
+
+    afterAll(() => {
+        // console.log("After ALL tests are execute");
+    });
+
+    test('should have counter == 0', () => {
+        expect(counter.get()).toBe(0);
+    });
+
+    test('counter should be 1 after increment', () => {
+        counter.increment();
+        expect(counter.get()).toBe(1);
+    });
 });
