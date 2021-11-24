90de2d00ae3ad7787acaa02d647ac6100e561078
Zhixian Ong <zhixian@hotmail.com>
2021-11-24 16:46:51 +0800

    working sample of UserService

--- projects/travel-api/services/UserService.js
+++ projects/travel-api/services/UserService.js
@@ -1,71 +1,34 @@
-// var fs = require('fs');
-// var util = require('util');
-var crypto = require('crypto');
-var buffer = require('buffer');
-const jwt = require('jsonwebtoken');
+var Sqlite3Service = require('./Sqlite3Service');
+// var {Sqlite3Service, inst} = require('./Sqlite3Service');
 
 class UserService {
 
-    constructor() {}
+    constructor() {
+        this.db = Sqlite3Service;
+    }
 
+    async addUser(username, password) {
+        return await this.db.execute(
+            `INSERT INTO app_user (username, password) VALUES (?, ?);`
+            , [username, password]
+        );
+    }
+
+    async updateUser(username, password) {
+        return await this.db.execute(
+            `UPDATE app_user SET password = ? WHERE username = ?;`
+            , [password, username]
+        );
+    }
+
+    
+    async deleteUser(username, password) {
+        return await this.db.execute(
+            `DELETE FROM app_user WHERE username = ?;`
+            , [username]
+        );
+    }
 }
 
-const svc = new UserService();
 
-if (require.main === module) {
-    
-    // console.log(module);     // module  -- info about current module
-    // console.log(require);    // require -- info about Node.js ecosystem
-
-    // Encrypt
-
-    let random32Bytes = crypto.randomBytes(32);
-
-    // console.log(`[HEX]:${random32Bytes.toString("hex")}`)
-
-    // console.log(`[B64]:${Buffer.from(random32Bytes).toString('base64')}`)
-
-    // Decrypt
-
-    let b64Bytes = 'OcaNtrP+QMBbIe8czpmT8LhIYzFi5qxbo6Iy1OVpX9Y=';
-
-    let rndBytes = Buffer.from(b64Bytes, 'base64');
-
-    let TOKEN_SECRET = rndBytes.toString("hex");
-
-    console.log(`[HEX]:${rndBytes.toString("hex")}`)
-
-    // Generate JWT
-    
-    let token = jwt.sign({ foo: 'ongzhixian' }, TOKEN_SECRET);
-    
-    console.log(token);
-
-    // Verify JWT
-
-    let tok = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb28iOiJvbmd6aGl4aWFuIiwiaWF0IjoxNjM3NTAwMjQzfQ.QaGwuqI4uyLijyRNSvf-A14Q3E8x4M1012c7-Y5ns8k';
-
-    jwt.verify(tok, TOKEN_SECRET, (err, user) => {
-
-        console.log(err)
-    
-        if (err) 
-            console.error(err);
-            // return res.sendStatus(403)
-    
-        req.user = user
-    
-        next()
-
-    });
-
-    const svc = new UserService();
-
-    // Convert to base64
-    // console.log(Buffer.from("Hello World").toString('base64'));
-    // Convert from base64
-    // console.log(Buffer.from("SGVsbG8gV29ybGQ=", 'base64').toString('utf8'));
-    
-}
-
-module.exports = svc;
+module.exports = new UserService();
