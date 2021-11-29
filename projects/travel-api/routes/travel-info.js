39d16724d7f1954d990ffb4ddfa308dc559d50e0
Zhixian Ong <zhixian@hotmail.com>
2021-11-29 10:53:36 +0800

    add stub for travel-info api

---
+++ projects/travel-api/routes/travel-info.js
@@ -0,0 +1,40 @@
+var router = require('express').Router();
+var fs = require("fs");
+var travelService = require('../services/TravelService');
+var countryService = require('../services/CountryService');
+var log = require('../services/LogService');
+
+// HTTP Verb    Path	                    Function
+// GET	        /api/travel-info/{id}       Return travel-info for id
+// POST	        /api/travel-info	        Add travel info country
+// PUT	        /api/travel-info/{id}	    Update travel info
+// DELETE	    /api/travel-info/{id}	    Delete travel info
+
+
+router.get('/:id', async function (req, res) {
+    
+    const PARAM_ID = 'id';
+    
+    let id = ''
+
+    if (PARAM_ID in req.params)
+        id = req.params[PARAM_ID];
+
+    res.send(`TODO: return travel info for id: [${id}]`);
+    // res.send(await countryService.mgGetCountry(id));
+    // res.send(await countryService.getCountry(id));
+})
+
+router.post('/', function (req, res) {
+    res.send('TODO: add country detail');
+});
+
+router.put('/:id', function (req, res) {
+    res.send('TODO: update country detail');
+});
+
+router.delete('/:id', function (req, res) {
+    res.send('TODO: update country detail');
+});
+
+module.exports = router;
